d38f06c4f2b00a0074006292941ab9ef3cf4aa07
lenah <lenaalsehli@yahoo.com>
2024-01-27 16:37:59 +0300

    Add project files.

---
+++ Majd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Majd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
